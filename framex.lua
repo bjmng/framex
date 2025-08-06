813d176f80396325f4a2145f5bdaf846d3c9b551
bjmng <benjamin-grill@outlook.com>
2025-08-06 20:24:35 +0200

    Add internationalization (i18n)

--- framex.lua
+++ framex.lua
@@ -12,6 +12,14 @@ function descriptor()
     }
 end
 
+local i18n = { language = "en" }
+
+i18n.dictionary = {
+    ["en"] = { ["fps"] = "Frame rate" }
+}
+
+i18n.t = function(key) return i18n.dictionary[key] or key end
+
 local fps = { value = nil, default = 25, mode = "undefined" }
 
 fps.detect = {}
@@ -23,7 +31,7 @@ fps.detect.from.info = function()
     local info = item:info()
     for _, entries in pairs(info) do
         for key, value in pairs(entries) do
-            if string.match(key, "Frame rate") then return tonumber(value) end
+            if string.match(key, i18n.t("fps")) then return tonumber(value) end
         end
     end
     return nil
@@ -49,6 +57,7 @@ end
 
 function activate()
     fps.detect.auto()
+    vlc.msg.info("fps: " .. fps.value .. " (" .. fps.mode .. ")")
 end
 
 function deactivate() end
