dd8c560e9a8aa41611c55717df503d5b2bd222fc
bjmng <benjamin-grill@outlook.com>
2025-08-06 18:51:51 +0200

    Add: descriptor

--- framex.lua
+++ framex.lua
@@ -1,1 +1,13 @@
 --[[ framex - VLC Frame eXtension ]]
+
+function descriptor()
+    return {
+        title = "framex",
+        version = "0.1.0",
+        author = "bjmng",
+        url = "https://github.com/bjmng/framex",
+        shortdesc = "Frame eXtension",
+        description = "Frame handling for the VLC Media Player.",
+        capabilities = { "close" }
+    }
+end
