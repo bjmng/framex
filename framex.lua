35af851aa21d71849c8921fd43ed950eecbbe18e
bjmng <benjamin-grill@outlook.com>
2025-08-07 22:18:34 +0200

    Add User Interface

--- framex.lua
+++ framex.lua
@@ -1,21 +1,22 @@
---[[ framex ]]
+--[[ framex © 2025, Benjamin Grill ]]
 
 function descriptor()
     return {
         title = "framex",
         version = "1.0",
-        author = "bjmng",
+        author = "Benjamin Grill",
         url = "https://github.com/bjmng/framex",
         shortdesc = "Frame eXtension",
         description = "Frame handling for the VLC Media Player.",
-        capabilities = { "close" }
+        capabilities = { "input-listener", "close" }
     }
 end
 
 local i18n = { language = "en" }
 
 i18n.dictionary = {
-    ["en"] = { ["fps"] = "Frame rate" }
+    ["en"] = { ["fps"] = "Frame rate" },
+    ["de"] = { ["fps"] = "Bildwiederholrate" }
 }
 
 i18n.t = function(key) return i18n.dictionary[i18n.language][key] or key end
@@ -59,11 +60,45 @@ fps.tostring = function()
     return i18n.t("fps") .. ": " .. tostring(fps.value) .. " (" .. fps.mode .. ")"
 end
 
-function activate()
-    fps.detect.auto()
-    vlc.msg.info(fps.tostring())
+local ui = { dialog = nil }
+
+ui.symbols = { reload = "⟳", play = "▷" }
+
+ui.label = {}
+ui.label.table = {}
+
+ui.label.add = function(key, value, x, y, w, h)
+    ui.label.table[key] = ui.dialog:add_label(value, x, y, w, h)
 end
 
-function deactivate() end
+ui.label.get = function(key) return ui.label.table[key] end
+
+ui.button = {}
+ui.button.table = {}
+
+ui.button.add = function(key, value, callback, x, y, w, h)
+    ui.button.table[key] = ui.dialog:add_button(value, callback, x, y, w, h)
+end
+
+ui.init = function()
+    ui.dialog = vlc.dialog(descriptor().title)
+    ui.label.add("fps", fps.tostring(), 1, 1, 1, 1)
+    ui.button.add("reload", ui.symbols.reload, update, 2, 1, 1, 1)
+end
+
+function activate()
+    ui.init()
+end
+
+function update()
+    fps.detect.auto()
+    ui.label.get("fps"):set_text(fps.tostring())
+end
+
+function input_updated() update() end
+
+function deactivate()
+    if ui.dialog then ui.dialog:delete() end
+end
 
 function close() deactivate() end
